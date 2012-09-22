64fd99c86e05020cae75f065db52d55de25918a4
Dylan Cochran <heliocentric@gmail.com>
2012-09-22 18:49:00 -0400

    * Comment out debug lines.

--- src/irc.c
+++ src/irc.c
@@ -352,7 +352,9 @@ void irc_send(int type, char *data, ...)
    if (send(IRC_FD, tosend, strlen(tosend), 0) == -1)
    {
       /* Return of -1 indicates error sending data; we reconnect. */
+/*
       log_printf("IRC -> Error sending data to server\n");
+*/
       irc_reconnect();
    }
 }
